a90c47c0fb208d0bc710c690432c666ac0da734a
yuichiro-nakano <u830977g@ecs.osaka-u.ac.jp>
2024-07-25 15:17:30 +0900

    run 20sites simulation

--- src/spectral_gap.py
+++ src/spectral_gap.py
@@ -213,7 +213,7 @@ if __name__ == '__main__':
     
     # instance
     source_dir_name = '../data/instance_set_2024-0614-1705-31'
-    n_spin = 11
+    n_spin = 12
     #beta_list = [1e1]
     beta_list = [1e-1, 1e0, 2e0, 5e0, 1e1]
     
